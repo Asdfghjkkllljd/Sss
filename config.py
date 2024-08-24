d344b30772164e559548adf6ce6a584883c502e7
Asdfghjkkllljd <staricinal49@gmail.com>
2024-08-24 17:17:24 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = "7429518608:AAFMU2R_CyyLqPhWl2LJTWxTQVPU1DhSppk" # Токен бота
+CHANNEL_ID = -1002243436081 # ID канала со ставками
+ADMINS = [5846969551] # ID админов
+BET_URL = "t.me/send?start=IVpAqLJ8Pztg" # Ссылка на счет
+CRYPTOPAY_TOKEN = "251801:AAaOA42R6B4OjqU4ciwBt2KRjmAHfyPx6mY" # Тут CryptoPay токен
+WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
+LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
+LOGS_ID = -1002226990800 # ID канала с оплатами
+
+RULES_LINK = "https://t.me/+k42zVBd8Iz41ZDEy" # Ссылка на канал с правилами
+NEWS_LINK = "https://t.me/+YXO5t3AYIRgxZTdi" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/CEKC_HA_OCTPOBAX" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "@extremecasinoo" # Ссылка на переходник
+BETS_LINK = "https://t.me/extremecasinoo" # Ссылка на канал ставок
+CASINO_NAME = "EXTREME CASINO" # Название казино
